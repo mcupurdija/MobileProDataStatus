a293f3c6b969a66e063978aa262b0db7a482a308
vladimirm <makevic@gmail.com>
2012-12-15 15:03:30 +0100

    Format changes to db sql.

--- mobile_store/assets/sql/create_mobile_store.sql
+++ mobile_store/assets/sql/create_mobile_store.sql
@@ -1,67 +1,64 @@
 CREATE TABLE `USERS` ( 
-       `_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-       `username` TEXT,
-       `pass` TEXT,
-       `sales_person_id` INTEGER,
-       `last_login` TEXT
+	`_id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`username` TEXT,
+	`pass` TEXT,
+	`sales_person_id` INTEGER,
+	`last_login` TEXT
 );
 INSERT INTO `USERS` (username,pass,sales_person_id, last_login) VALUES 
-		('tica', 'tica', 1, 'yesterday'); 
+	('tica', 'tica', 1, 'yesterday'); 
 
 CREATE TABLE `invoices` (
-		`_id` INTEGER PRIMARY KEY  NOT NULL ,
-		`no` TEXT NOT NULL  DEFAULT (0) ,
-		`customer_id` INTEGER,
-		`posting_date` TEXT,
-		`sales_person_id` INTEGER,
-		`due_date` TEXT,
-		`total` REAL,
-		`total_left` REAL,
-		`due_date_days_left` INTEGER,
-		`created_date` TEXT,
-		`created_by` TEXT,
-		`updated_date` TEXT,
-		`updated_by`TEXT
+	`_id` INTEGER PRIMARY KEY  NOT NULL ,
+	`no` TEXT NOT NULL  DEFAULT (0) ,
+	`customer_id` INTEGER,
+	`posting_date` TEXT,
+	`sales_person_id` INTEGER,
+	`due_date` TEXT,
+	`total` REAL,
+	`total_left` REAL,
+	`due_date_days_left` INTEGER,
+	`created_date` TEXT,
+	`created_by` TEXT,
+	`updated_date` TEXT,
+	`updated_by`TEXT
 );
 INSERT INTO `invoices` (`_id` ,	`no`  ,	`customer_id`,`posting_date` ,`sales_person_id`,`due_date` ,`total` ,`total_left` ,	`due_date_days_left` ,`created_date` ,`created_by` ,`updated_date`,	`updated_by`) VALUES
-		(1 , '2012-12-12_NO_1', 12 , '2012-11-10 08:08:00.000', 1 , '2012-12-12', 1222 , 100 , 5 , '2012-11-10 08:08:00.000', 'system', '2012-11-11', 'sytem' );
+	(1 , '2012-12-12_NO_1', 12 , '2012-11-10 08:08:00.000', 1 , '2012-12-12', 1222 , 100 , 5 , '2012-11-10 08:08:00.000', 'system', '2012-11-11', 'sytem' );
 INSERT INTO `invoices` (`_id` ,	`no`  ,	`customer_id`,`posting_date` ,`sales_person_id`,`due_date` ,`total` ,`total_left` ,	`due_date_days_left` ,`created_date` ,`created_by` ,`updated_date`,	`updated_by`) VALUES
-		(2 , '2011-12-12_NO_4', 13 , '2011-11-12 14:00:00.000', 1 , '2011-12-12', 22 , 22 , 2 , '2011-11-12 14:00:00.000', 'system', '2011-11-11', 'sytem');
-		
+	(2 , '2011-12-12_NO_4', 13 , '2011-11-12 14:00:00.000', 1 , '2011-12-12', 22 , 22 , 2 , '2011-11-12 14:00:00.000', 'system', '2011-11-11', 'sytem');
 INSERT INTO `invoices` (`_id` ,	`no`  ,	`customer_id`,`posting_date` ,`sales_person_id`,`due_date` ,`total` ,`total_left` ,	`due_date_days_left` ,`created_date` ,`created_by` ,`updated_date`,	`updated_by`) VALUES
-		(3 , '2011-12-12_NO_1', 13 , '2011-11-12 16:00:00.000', 1 , '2011-12-12', 411 , 411 , 2 , '2011-11-12 16:00:00.000', 'system', '2011-11-11', 'sytem');
-
+	(3 , '2011-12-12_NO_1', 13 , '2011-11-12 16:00:00.000', 1 , '2011-12-12', 411 , 411 , 2 , '2011-11-12 16:00:00.000', 'system', '2011-11-11', 'sytem');
 INSERT INTO `invoices` (`_id` ,	`no`  ,	`customer_id`,`posting_date` ,`sales_person_id`,`due_date` ,`total` ,`total_left` ,	`due_date_days_left` ,`created_date` ,`created_by` ,`updated_date`,	`updated_by`) VALUES
-		(4 , '2012-12-12_NO_6', 12 , '2012-11-10 05:08:00.000', 1 , '2012-12-12', 1222 , 100 , 5 , '2012-11-10 05:08:00.000', 'system', '2012-11-11', 'sytem' );
+	(4 , '2012-12-12_NO_6', 12 , '2012-11-10 05:08:00.000', 1 , '2012-12-12', 1222 , 100 , 5 , '2012-11-10 05:08:00.000', 'system', '2012-11-11', 'sytem' );
 INSERT INTO `invoices` (`_id` ,	`no`  ,	`customer_id`,`posting_date` ,`sales_person_id`,`due_date` ,`total` ,`total_left` ,	`due_date_days_left` ,`created_date` ,`created_by` ,`updated_date`,	`updated_by`) VALUES
-		(5 , '2011-12-12_NO_5', 13 , '2011-11-12 15:00:00.000', 1 , '2011-12-12', 22 , 22 , 2 , '2011-11-12 15:00:00.000', 'system', '2011-11-11', 'sytem');
-		
+	(5 , '2011-12-12_NO_5', 13 , '2011-11-12 15:00:00.000', 1 , '2011-12-12', 22 , 22 , 2 , '2011-11-12 15:00:00.000', 'system', '2011-11-11', 'sytem');
 INSERT INTO `invoices` (`_id` ,	`no`  ,	`customer_id`,`posting_date` ,`sales_person_id`,`due_date` ,`total` ,`total_left` ,	`due_date_days_left` ,`created_date` ,`created_by` ,`updated_date`,	`updated_by`) VALUES
-		(6 , '2011-12-12_NO_3', 13 ,'2011-11-16 16:00:00.000', 1 , '2011-12-12', 411 , 411 , 2 , '2011-11-16 16:00:00.000', 'system', '2011-11-11', 'sytem');
-		
-		
+	(6 , '2011-12-12_NO_3', 13 ,'2011-11-16 16:00:00.000', 1 , '2011-12-12', 411 , 411 , 2 , '2011-11-16 16:00:00.000', 'system', '2011-11-11', 'sytem');
+	
 CREATE TABLE `customers` (
-			`_id` INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , 
-			`no` INTEGER, 
-			`name` TEXT, 
-			`name2` TEXT, 
-			`address_id` INTEGER, 
-			`city` TEXT, 
-			`post_code` TEXT, 
-			`phone` TEXT, 
-			`mobile` TEXT, 
-			`email` TEXT, 
-			`company_id` INTEGER, 
-			`primary_contact_id` INTEGER, 
-			`vat_reg_no` TEXT, 
-			`credit_limit_lcy` REAL, 
-			`balance_lcy` REAL, 
-			`balance_due_lcy` REAL, 
-			`payment_terms_code` INTEGER, 
-			`priority` INTEGER, 
-			`global_dimension` INTEGER, 
-			`channel_oran` TEXT,
-			`blocked_status` TEXT);
+	`_id` INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , 
+	`no` INTEGER, 
+	`name` TEXT, 
+	`name2` TEXT, 
+	`address_id` INTEGER, 
+	`city` TEXT, 
+	`post_code` TEXT, 
+	`phone` TEXT, 
+	`mobile` TEXT, 
+	`email` TEXT, 
+	`company_id` INTEGER, 
+	`primary_contact_id` INTEGER, 
+	`vat_reg_no` TEXT, 
+	`credit_limit_lcy` REAL, 
+	`balance_lcy` REAL, 
+	`balance_due_lcy` REAL, 
+	`payment_terms_code` INTEGER, 
+	`priority` INTEGER, 
+	`global_dimension` INTEGER, 
+	`channel_oran` TEXT,
+	`blocked_status` TEXT
+);
 			
 			
 INSERT INTO  `customers` (`_id`,`no` , `name` , `name2` , 	`address_id` , 	`city` , `post_code` , 	`phone` , `mobile` , `email` , `company_id` , `primary_contact_id` , `vat_reg_no` , `credit_limit_lcy` , `balance_lcy` , `balance_due_lcy` , `payment_terms_code` , `priority` , `global_dimension` , `channel_oran`,`blocked_status` ) VALUES 
