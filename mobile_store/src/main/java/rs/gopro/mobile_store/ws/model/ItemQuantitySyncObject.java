4064baf02a6f3d3c40894077a85d72bbc8b7c5f2
vladimirm <makevic@gmail.com>
2013-01-09 17:19:32 +0100

    Test case and some comments.

--- mobile_store/src/main/java/rs/gopro/mobile_store/ws/model/ItemQuantitySyncObject.java
+++ mobile_store/src/main/java/rs/gopro/mobile_store/ws/model/ItemQuantitySyncObject.java
@@ -127,7 +127,7 @@ public class ItemQuantitySyncObject extends SyncObject {
 
 	@Override
 	public void logSyncEnd(ContentResolver contentResolver) {
-		
+		// TODO Auto-generated method stub
 	}
 
 	public String getmItemNoa46() {
