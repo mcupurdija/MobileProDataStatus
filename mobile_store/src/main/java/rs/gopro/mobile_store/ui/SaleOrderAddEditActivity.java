a3689c8311572a7c6270b3720a018a65ac5cdf0f
vladimirm <makevic@gmail.com>
2013-01-12 12:14:09 +0100

    Stage commit for creation of sale orders.

--- mobile_store/src/main/java/rs/gopro/mobile_store/ui/SaleOrderAddEditActivity.java
+++ mobile_store/src/main/java/rs/gopro/mobile_store/ui/SaleOrderAddEditActivity.java
@@ -1,6 +1,8 @@
 package rs.gopro.mobile_store.ui;
 
 import rs.gopro.mobile_store.R;
+import rs.gopro.mobile_store.provider.MobileStoreContract;
+import rs.gopro.mobile_store.util.LogUtils;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
@@ -10,6 +12,15 @@ import android.support.v4.content.Loader;
 
 public class SaleOrderAddEditActivity  extends BaseActivity implements LoaderCallbacks<Cursor> {
 
+	private static final String TAG = "SaleOrderAddEditActivity";
+	
+	private static String DEFAULT_VIEW_TYPE = MobileStoreContract.SaleOrders.CONTENT_ITEM_TYPE;
+	
+	private int mState;
+    private Uri mUri;
+    private Cursor mCursor;
+    private String mViewType;
+	
 	public SaleOrderAddEditActivity() {
 		// TODO Auto-generated constructor stub
 	}
@@ -18,33 +29,51 @@ public class SaleOrderAddEditActivity  extends BaseActivity implements LoaderCal
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		setContentView(R.layout.activity_add_visit);
+		setContentView(R.layout.activity_add_sale_order);
 		
 		// routes data from intent that called this activity to business logic
 		routeIntent(getIntent(), savedInstanceState != null);
 	}
 	
+	/**
+	 * Routes activity on passed data through intent.
+	 * It is ran only in onCreate().
+	 * @param intent
+	 * @param b
+	 */
 	private void routeIntent(Intent intent, boolean b) {
 		// get action from intent
 		String action = intent.getAction();
 		if (action == null) {
+			LogUtils.LOGE(TAG, "Activity called without action!");
 			return;
 		}
 		// get URI from intent
 		Uri uri = intent.getData();
 		if (uri == null) {
+			LogUtils.LOGE(TAG, "Activity called without URI!");
+			return;
+		}
+		// check uri mime type
+		mViewType = getContentResolver().getType(uri);
+		// wrong uri, only working with single item
+		if (!mViewType.equals(DEFAULT_VIEW_TYPE)) {
+			LogUtils.LOGE(TAG, "Activity called with wrong URI! URI:"+uri.toString());
 			return;
 		}
 		
-		String mimeType = getContentResolver().getType(uri);
-		
+		// check action and route from there
 		if (Intent.ACTION_EDIT.equals(action)) {
-			
+			initComponents(action);
 		} else if (Intent.ACTION_INSERT.equals(action)) {
-			
-		} else {
-			
+			initComponents(action);
+		} else if (Intent.ACTION_VIEW.equals(action))  {
+			initComponents(action);
 		}
+	}
+
+	private void initComponents(String action) {
+		
 	}
 
 	@Override
